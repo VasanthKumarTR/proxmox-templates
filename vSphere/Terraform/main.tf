090c4770f96f491d1a1ff3eee52624604b001299
Sam Gabriail <your-email@example.com>
2025-05-22 11:29:53 -0400

    working terraform vsphere config

--- vSphere/Terraform/main.tf
+++ vSphere/Terraform/main.tf
@@ -2,61 +2,64 @@ terraform {
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = "~> 2.3.1"
+      version = "2.12.0"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
-  vsphere_server       = var.vsphere_server
-  allow_unverified_ssl = var.allow_unverified_ssl
+  vsphere_server       = var.vsphere_vcenter
+  allow_unverified_ssl = true
 }
 
+locals {
+  templatevars = {
+    name         = var.name,
+    ipv4_address = var.ipv4_address,
+    ipv4_gateway = var.ipv4_gateway,
+    dns_server_1 = var.dns_server_list[0],
+    dns_server_2 = var.dns_server_list[1],
+    public_key   = var.public_key,
+    ssh_username = var.ssh_username
+  }
+}
+
+# Define VMware vSphere 
 data "vsphere_datacenter" "dc" {
-  name = var.datacenter
+  name = var.vsphere-datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = var.datastore
+  name          = var.vm-datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = var.cluster
+  name          = var.vsphere-cluster
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = var.network
+  name          = var.vm-network
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = var.template_name
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-resource "vsphere_folder" "folder" {
-  path          = var.vm_folder
-  type          = "vm"
+  name          = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  count            = var.vm_count
-  name             = "${var.vm_name_prefix}-${count.index + 1}"
+  name             = var.name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  folder           = vsphere_folder.folder.path
 
-  num_cpus = var.num_cpus
-  memory   = var.memory
-  guest_id = data.vsphere_virtual_machine.template.guest_id
-
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  num_cpus             = var.cpu
+  num_cores_per_socket = var.cores-per-socket
+  memory               = var.ram
+  guest_id             = var.vm-guest-id
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -64,85 +67,29 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   disk {
-    label            = "disk0"
-    size             = var.disk_size == 0 ? data.vsphere_virtual_machine.template.disks[0].size : var.disk_size
-    thin_provisioned = true
+    label            = "${var.name}-disk"
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    size             = var.disksize == "" ? data.vsphere_virtual_machine.template.disks.0.size : var.disksize
   }
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
-
-    customize {
-      linux_options {
-        host_name = "${var.vm_name_prefix}-${count.index + 1}"
-        domain    = var.domain
-      }
-
-      network_interface {}
-    }
   }
-
-  # Extra configuration drive for cloud-init
   extra_config = {
-    "guestinfo.userdata" = base64encode(templatefile("${path.module}/cloud-init/user-data.tftpl", {
-      hostname          = "${var.vm_name_prefix}-${count.index + 1}"
-      ssh_public_key    = var.ssh_public_key != "" ? var.ssh_public_key : file(pathexpand(var.ssh_public_key_path))
-      override_password = var.override_password
-      vm_password       = var.vm_password
-    }))
-    "guestinfo.userdata.encoding" = "base64"
-    "guestinfo.metadata" = base64encode(templatefile("${path.module}/cloud-init/meta-data.tftpl", {
-      hostname    = "${var.vm_name_prefix}-${count.index + 1}"
-      instance_id = "${var.vm_name_prefix}-${count.index + 1}-${random_uuid.instance_id[count.index].result}"
-    }))
+    "guestinfo.metadata"          = base64encode(templatefile("${path.module}/templates/metadata.yaml", local.templatevars))
     "guestinfo.metadata.encoding" = "base64"
+    "guestinfo.userdata"          = base64encode(templatefile("${path.module}/templates/userdata.yaml", local.templatevars))
+    "guestinfo.userdata.encoding" = "base64"
   }
-
   lifecycle {
     ignore_changes = [
-      annotation
+      annotation,
+      clone[0].template_uuid,
+      clone[0].customize[0].dns_server_list,
+      clone[0].customize[0].network_interface[0]
     ]
   }
-
-  # Wait for VMware tools to come online
-  wait_for_guest_net_timeout = 10
-  wait_for_guest_ip_timeout  = 10
-
-  # Setup SSH connection for remote-exec
-  connection {
-    type        = "ssh"
-    user        = var.ssh_username
-    private_key = file(var.ssh_private_key_path)
-    host        = self.default_ip_address
-  }
-
-  # Run post-deployment configuration
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Setting hostname...'",
-      "sudo hostnamectl set-hostname ${var.vm_name_prefix}-${count.index + 1}.${var.domain}",
-      "echo 'Updating system...'",
-      "sudo apt-get update && sudo apt-get upgrade -y",
-      "echo 'Installation complete!' > ~/setup-complete.log"
-    ]
-  }
-}
-
-# Generate a unique instance ID for each VM for cloud-init
-resource "random_uuid" "instance_id" {
-  count = var.vm_count
-}
-
-output "vm_ips" {
-  value = {
-    for idx, vm in vsphere_virtual_machine.vm : vm.name => vm.default_ip_address
-  }
-  description = "The IP addresses of the created VMs"
-}
-
-output "vm_ids" {
-  value = {
-    for idx, vm in vsphere_virtual_machine.vm : vm.name => vm.id
-  }
-  description = "The IDs of the created VMs"
+  firmware                = "efi"
+  efi_secure_boot_enabled = false
 }
--- vSphere/Terraform/variables.tf
+++ vSphere/Terraform/variables.tf
@@ -1,123 +1,140 @@
+#===========================#
+# VMware vCenter connection #
+#===========================#
+
 variable "vsphere_user" {
   type        = string
-  description = "vSphere username"
+  description = "VMware vSphere user name"
+  sensitive = true
 }
 
 variable "vsphere_password" {
   type        = string
-  sensitive   = true
-  description = "vSphere password"
+  description = "VMware vSphere password"
+  sensitive = true
 }
 
-variable "vsphere_server" {
+variable "vsphere_vcenter" {
   type        = string
-  description = "vSphere server address"
+  description = "VMWare vCenter server FQDN / IP"
+  sensitive = true
 }
 
-variable "allow_unverified_ssl" {
-  type        = bool
-  default     = true
-  description = "Allow unverified SSL certificate"
-}
-
-variable "datacenter" {
+variable "vsphere-unverified-ssl" {
   type        = string
-  description = "vSphere datacenter name"
+  description = "Is the VMware vCenter using a self signed certificate (true/false)"
 }
 
-variable "datastore" {
+variable "vsphere-datacenter" {
   type        = string
-  description = "vSphere datastore name"
+  description = "VMWare vSphere datacenter"
 }
 
-variable "cluster" {
+variable "vsphere-cluster" {
   type        = string
-  description = "vSphere cluster name"
+  description = "VMWare vSphere cluster"
+  default     = ""
 }
 
-variable "network" {
+variable "vsphere-template-folder" {
   type        = string
-  description = "vSphere network name"
+  description = "Template folder"
+  default = "Templates"
 }
 
-variable "template_name" {
+#================================#
+# VMware vSphere virtual machine #
+#================================#
+
+variable "name" {
   type        = string
-  description = "VM template name to clone from"
+  description = "The name of the vSphere virtual machines and the hostname of the machine"
 }
 
-variable "vm_folder" {
+variable "vm-name-prefix" {
   type        = string
-  description = "VM folder to create VMs in"
+  description = "Name of VM prefix"
+  default     =  "k3sup"
 }
 
-variable "vm_name_prefix" {
+variable "vm-datastore" {
   type        = string
-  default     = "ubuntu24"
-  description = "Prefix for VM names"
+  description = "Datastore used for the vSphere virtual machines"
 }
 
-variable "domain" {
+variable "vm-network" {
   type        = string
-  default     = "local"
-  description = "Domain name for VMs"
+  description = "Network used for the vSphere virtual machines"
 }
 
-variable "vm_count" {
-  type        = number
-  default     = 1
-  description = "Number of VMs to create"
+variable "vm-linked-clone" {
+  type        = string
+  description = "Use linked clone to create the vSphere virtual machine from the template (true/false). If you would like to use the linked clone feature, your template need to have one and only one snapshot"
+  default     = "false"
 }
 
-variable "num_cpus" {
-  type        = number
+variable "cpu" {
+  description = "Number of vCPU for the vSphere virtual machines"
   default     = 2
-  description = "Number of vCPUs for the VM"
 }
 
-variable "memory" {
-  type        = number
-  default     = 2048
-  description = "Memory in MB for the VM"
+variable "cores-per-socket" {
+  description = "Number of cores per cpu for workers"
+  default     = 1
 }
 
-variable "disk_size" {
-  type        = number
-  default     = 0
-  description = "Disk size in GB (0 means use template's disk size)"
+variable "ram" {
+  description = "Amount of RAM for the vSphere virtual machines (example: 2048)"
+}
+
+variable "disksize" {
+  description = "Disk size, example 100 for 100 GB"
+  default = ""
+}
+
+variable "vm-guest-id" {
+  type        = string
+  description = "The ID of virtual machines operating system"
+}
+
+variable "vm-template-name" {
+  type        = string
+  description = "The template to clone to create the VM"
+}
+
+variable "vm-domain" {
+  type        = string
+  description = "Linux virtual machine domain name for the machine. This, along with host_name, make up the FQDN of the virtual machine"
+  default     = ""
+}
+
+variable "dns_server_list" {
+  type = list(string)
+  description = "List of DNS servers"
+  default = ["8.8.8.8", "8.8.4.4"]
+}
+
+variable "ipv4_address" {
+  type = string
+  description = "ipv4 addresses for a vm"
+}
+
+variable "ipv4_gateway" {
+  type = string
+}
+
+variable "ipv4_netmask" {
+  type = string
 }
 
 variable "ssh_username" {
-  type        = string
-  default     = "ubuntu"
-  description = "SSH username for remote access"
+  type      = string
+  sensitive = true
+  default   = "sam"
 }
 
-variable "ssh_private_key_path" {
+variable "public_key" {
   type        = string
-  description = "Path to SSH private key for remote access"
-}
-
-variable "ssh_public_key" {
-  type        = string
-  default     = ""
-  description = "SSH public key for authentication (if empty, will use ssh_public_key_path)"
-}
-
-variable "ssh_public_key_path" {
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-  description = "Path to SSH public key file (used if ssh_public_key is empty)"
-}
-
-variable "override_password" {
-  type        = bool
-  default     = false
-  description = "Whether to override the template's password"
-}
-
-variable "vm_password" {
-  type        = string
-  sensitive   = true
-  default     = ""
-  description = "Password for the VM user (only used if override_password is true)"
+  description = "Public key to be used to ssh into a machine"
+  default     = "AAAAB3NzaC1yc2EAAAADAQABAAABAQCb7fcDZfIG+SxuP5UsZaoHPdh9MNxtEL5xRI71hzMS5h4SsZiPGEP4shLcF9YxSncdOJpyOJ6OgumNSFWj2pCd/kqg9wQzk/E1o+FRMbWX5gX8xMzPig8mmKkW5szhnP+yYYYuGUqvTAKX4ua1mQwL6PipWKYJ1huJhgpGHrvSQ6kuywJ23hw4klcaiZKXVYtvTi8pqZHhE5Kx1237a/6GRwnbGLEp0UR2Q/KPf6yRgZIrCdD+AtOznSBsBhf5vqcfnnwEIC/DOnqcOTahBVtFhOKuPSv3bUikAD4Vw7SIRteMltUVkd/O341fx+diKOBY7a8M6pn81HEZEmGsr7rT sam@SamMac.local"
 }
